5454241cad035e29e1e558e7911a7861f58dc1e2
Deyang <zhaodeyang8811@gmail.com>
2016-04-18 00:23:28 -0700

    tf-ifd example code

--- ir_query_engine/retrieve_match_models/tf_idf_feature/transform.py
+++ ir_query_engine/retrieve_match_models/tf_idf_feature/transform.py
@@ -1,17 +1,12 @@
-from nltk.tokenize import RegexpTokenizer
-from stop_words import get_stop_words
-from nltk.stem.porter import PorterStemmer
 from gensim import corpora, models, similarities
-import os
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
 
 __author__ = 'Deyang'
 
+
+
 tokenizer = RegexpTokenizer(r'\w+')
-
-# create English stop words list
-en_stop = get_stop_words('en')
-
-# Create p_stemmer of class PorterStemmer
 p_stemmer = PorterStemmer()
 
 # create sample documents
@@ -24,29 +19,20 @@ doc_e = "Health professionals say that brocolli is good for your health."
 # compile sample documents into a list
 doc_set = [doc_a, doc_b, doc_c, doc_d, doc_e]
 
-
 def pre_process_doc(raw_doc):
+    """
+    Pre-processing fortf-idf does not
+    1) rm stop words
+    2) rm low-fre words
+
+    :param raw_doc:
+    :return:
+    """
     # clean and tokenize document string
     tokens = tokenizer.tokenize(raw_doc.lower())
-
-    # remove stop words from tokens
-    stopped_tokens = [t for t in tokens if t not in en_stop]
-
     # stem tokens
-    stemmed_tokens = [p_stemmer.stem(t) for t in stopped_tokens]
+    stemmed_tokens = [p_stemmer.stem(t) for t in tokens]
     return stemmed_tokens
-
-
-def get_model_file_path():
-    filename = 'lda.md'
-    dirpath = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(dirpath, filename)
-
-
-def get_dictionary_file_path():
-    filename = 'lda.dict'
-    dirpath = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(dirpath, filename)
 
 
 def docs_to_corpus(doc_set):
@@ -66,34 +52,20 @@ def docs_to_corpus(doc_set):
 
 
 
-
 dictionary, corpus = docs_to_corpus(doc_set)
-
-# generate LDA model
-ldamodel = models.ldamodel.LdaModel(corpus, num_topics=3, id2word=dictionary, passes=20)
-
-
-
-predict = ldamodel[dictionary.doc2bow(pre_process_doc(doc_a))]
-print predict
-
-
-ldamodel.save(get_model_file_path())
-
-
-####### test save and load
-dictionary.save_as_text(get_dictionary_file_path())
+print dictionary
+print corpus
+tfidf = models.TfidfModel(corpus)
 
 
 
-# the real query part
-new_dict = corpora.Dictionary.load_from_text(get_dictionary_file_path())
-new_model = models.ldamodel.LdaModel.load(get_model_file_path())
+# query part
+doc_f = "brocolli is good for healthy."
 
-predict = new_model[new_dict.doc2bow(pre_process_doc(doc_a))]
-print predict
+vec = tfidf[dictionary.doc2bow(pre_process_doc(doc_f))]
+
+index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=47)
 
 
-index = similarities.MatrixSimilarity(new_model[corpus], num_features=3)
-
-print index[predict]
+sims = index[vec]
+print(list(enumerate(sims)))
